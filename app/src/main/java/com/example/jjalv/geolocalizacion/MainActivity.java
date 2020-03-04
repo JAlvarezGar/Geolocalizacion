2c7d0bfde0fb062a38fa4868925c90c4c4ed4052
JAlvarezGar <jjalvarezgar@gmail.com>
2020-03-04 14:05:12 +0100

    funcionando solo muestra la altitud y la latitud en un campo de texto

--- app/src/main/java/com/example/jjalv/geolocalizacion/MainActivity.java
+++ app/src/main/java/com/example/jjalv/geolocalizacion/MainActivity.java
@@ -1,17 +1,93 @@
 package com.example.jjalv.geolocalizacion;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.location.*;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    Button bt1;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    private LocationManager location;
+    private String posicion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt1=(Button)findViewById(R.id.button);
+        
+        
+        locationManager=(LocationManager)getSystemService(LOCATION_SERVICE);
+
+        // CRITERIOS QUE DEBE CUMPLIR EL PROVEEDOR DE LOCALIZACION
+        Criteria criteria= new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+        // SE DEJA QUE ANDROID ELIJA EL MEJOR PROVEEDOR
+        String proveedor= locationManager.getBestProvider(criteria,true);
+        
+
+        
+        
+
+
+        locationListener= new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                
+                // COMO HA CAMBIADO LA UBICACION 
+                // HAY QUE AVISAR AL USUARIO
+                actualizarLocalizacion(location);
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        // ACTULIZA POSICION CADA 5 SEGUNDOS Y CADA 0 METROS
+        locationManager.requestLocationUpdates("gps",5000,0,locationListener);
+
+    }
+
+    private void actualizarLocalizacion(Location location) {
+
+        TextView tvLocalizacion;
+        tvLocalizacion=(TextView)findViewById(R.id.textLocalizacion);
+        
+        if(location!=null){
+            double lat=location.getLatitude();
+            double alt= location.getAltitude();
+            posicion=("Latitud : "+lat+"\nAltitud : "+alt);
+        }
+        tvLocalizacion.setText(posicion);
+        
     }
 
 
